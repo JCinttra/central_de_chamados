d22c74f714e1e0a995bd0af7519a4573e7adf658
JCinttra <joaopedro161001@gmail.com>
2026-09-08 17:53:39 -0300

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Configure este arquivo com os dados do seu projeto Firebase Web.
 // Os valores podem ser obtidos em Firebase Console > Configurações do projeto > Seus apps > Web.
 const firebaseConfig = {
-  apiKey: "COLE_AQUI_SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJETO.firebasestorage.app",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAjV7F_aKwgCzm0YXRhRBnZ3mzRNPjKxoc",
+  authDomain: "central-de-chamados-pcp.firebaseapp.com",
+  projectId: "central-de-chamados-pcp",
+  storageBucket: "central-de-chamados-pcp.firebasestorage.app",
+  messagingSenderId: "186523896419",
+  appId: "1:186523896419:web:111990a7cb8b05b4634656"
 };
 
 firebase.initializeApp(firebaseConfig);
